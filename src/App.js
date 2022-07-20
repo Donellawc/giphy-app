71bef7ba230fd8b09769605de8e8a5a00488e99f
Donella Cooper <donellawc@gmail.com>
2022-07-20 14:10:03 -0500

    routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import './App.css';
+import Menu from "./components/Menu";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import Register from "./components/RegisterPage";
+import SearchPage from "./componentsSearchPage";
+import FavoritesPage from "./components/FavoritesPage";
+
+
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Menu />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="*/" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
