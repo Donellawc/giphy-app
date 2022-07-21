67f798c46eb643dfba600502041f33b821e4eeea
Donella Cooper <donellawc@gmail.com>
2022-07-21 14:58:01 -0500

    axios drama

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -8,9 +8,7 @@ function  useAxios (url) {
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
-  
-
-
+    const baseUrl = "https://api.giphy.com/v1/gifs/search?api_key=FNjv7NV5aauCcH2UnwD07V2AXHPpy1xA&limit=100&offset=0&lang=en&rating=";
 
 
     return (
