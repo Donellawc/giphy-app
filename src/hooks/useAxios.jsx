e7d785949fa7f1f9c2b73b13c5726132cd09c4c6
Donella Cooper <donellawc@gmail.com>
2022-07-21 15:00:50 -0500

    axios

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,18 +1,38 @@
 import React, { useState, useEffect } from "react";
-import  Axios  from "axios";
+import  axios  from "axios";
 
 
+const baseUrl = "https://api.giphy.com/v1/gifs/search?api_key=FNjv7NV5aauCcH2UnwD07V2AXHPpy1xA&limit=100&offset=0&lang=en&rating=";
 
-function  useAxios (url) {
-
+function  useAxios (url) {        
+          
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(true);
-    const baseUrl = "https://api.giphy.com/v1/gifs/search?api_key=FNjv7NV5aauCcH2UnwD07V2AXHPpy1xA&limit=100&offset=0&lang=en&rating=";
 
+    useEffect(() => {
+    async function init() {        
+        setLoading(true);
+        setError(null);
+        setData();
 
-    return (
+        try {
+        const response = await axios.get(baseUrl + url);
+            setData(response.data)
 
-    )
+        } catch(e) {
+
+            setError(e);
+
+        } finally {
+            setLoading(false);
+        }
+    }
+    init();
+
+   },[url])
+        
+    return { data, error,loading };
 }
+
 export default useAxios
