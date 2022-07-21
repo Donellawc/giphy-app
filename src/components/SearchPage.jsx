3bf2d15986249896e3cce93b1418d7e4a8050c16
Donella Cooper <donellawc@gmail.com>
2022-07-21 12:09:53 -0500

    starting axios build

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,39 +1,36 @@
-import React, {useState, createContext} from "react";
+import React, { useState, createContext } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { SearchContext } from "../context/SearchContex";
 
 function SearchPage() {
 
 
-    const navigate = useNavigate ();
-    const [searchResults, setSearchresults] = useState(null);
-    const [rating, setRating] = useState(null);
+    const navigate = useNavigate();
+    const [searchResults, setSearchresults] = useState([]);
+    const [rating, setRating] = useState();
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
 
-  
+
+
     return (
-     <form>
-        <label htmlFor="search-bar">Search</label>
-        <input 
-        value={searchResults}
-        
-    
-
-        type="search"
-         name="" 
-         id="search-bar" 
-         />
-<button
-    type="submit"
-    onClick={ ()=> {
-
-    }
-
-    }
-    >
-
-</button>
-     </form>
-  )
+        <form>
+            <label htmlFor="search-bar">Search</label>
+            <input
+                value={searchResults}
+                onChange={(e) => {}}
+                type="search"
+                name="searchBar"
+                id="search-bar"
+            />
+            <button
+                type="submit"
+                onClick={(e) => {}}>
+                    Submit
+            </button>
+        </form>
+    )
 }
 
 export default SearchPage;
